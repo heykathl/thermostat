1f20f4f0f15db39f9835de9ef2fb2ae914855297
Rob Oman <rob.oman@icloud.com>
2022-03-31 11:40:11 +0100

    api demo
    Co-authored-by Kathleen Ly <heykathl@users.noreply.github.com>

---
+++ github.js
@@ -0,0 +1,12 @@
+import got from 'got';
+
+const fetchRepoInfo = (repoName, callback) => {
+  got(`https://api.github.com/repos/${repoName}`).then((response) => {
+    const responseObject = JSON.parse(response.body);
+    callback(responseObject);
+  });
+}
+
+fetchRepoInfo('sinatra/sinatra', (repoResponseObject) => {
+  console.log(repoResponseObject);
+});
